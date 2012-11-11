a7a634adac98fb69f7814d71453192897b3e5012
Kenya <kenya.vs@gmail.com>
2012-11-11 18:51:38 -0500

    added base64. refactored conversion functionality

--- js/conversion.js
+++ js/conversion.js
@@ -31,151 +31,93 @@ var tooltip = tooltipElements[0];
 	}
 
 	/**
- 	* Calculates the hexidecimal(base 16) of a decimal(base 10) number.
- 	* @param {number} val The desired decimal number to be converted.
+ 	* Calculates the given base number of a decimal(base 10) number.
+ 	* @param {number} base, The base value to convert to.
+ 	* @param {number} val, The desired decimal number to be converted.
  	* @return {string} Returns a string representation of a hexidecimal number.
  	*/
-	function getHex(val){
-		var hex = '';
-		
-		if(val>=0 && val<16){
-			hex+=lookupHex(Math.floor(val));
-			hex+=" ";
-			return hex;
-		}
-		else{
-			var x = Math.floor(val/16);
-			var y = val%16;
+	function getBaseX(base, val){
 
-			hex+=lookupHex(x);
-			hex+=lookupHex(y);
-			hex+=" ";
+		switch(base){
+			case 16:
+			case 32:
+			case 64:
+				//convert(base, val);
+				var num = '';
 
-			return hex;
+				if(val>=0 && val<base){
+					num+=lookup(base, Math.floor(val));
+					num+=" ";
+					return num;
+				}
+				else{
+					var x = Math.floor(val/base);
+					var y = val%base;
+
+					num+=lookup(base, x);
+					num+=lookup(base, y);
+					num+=" ";
+
+					return num;
+				}
+				break;
+
+			default:
+				console.log("Invalid base. Acceptable bases are 16, 32, and 64");
 		}
 	}
 
 	/**
- 	* Maps a given decimal(base 10) value with its corresponding hexidecimal representation.
- 	* @param {number} val The desired decimal number to map.
- 	* @return {string} Returns a single letter or number.
+ 	* Looks up the corresponding value for a given decimal number and base.
+ 	* @param {number} base, The base value to convert to.
+ 	* @param {number} val, The desired decimal number to be converted.
+ 	* @return {string} Returns a single letter or decimal number.
  	*/
-	function lookupHex(val){
-		var hex_lookup = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'};
+	function lookup(base, val){
+		var lookup = getLookup(base, val);
 
 		if(val<0){
 			console.log("Invalid input. Number must be positive");
 		}
-		else if(val<10){
+		else if(base==16 && val<10){//special case for hexidecimal. it might be better to just omit this condition and make have the base 16 lookup table mimic the others...satisfying case 0-9?
 			return val;
 		}
-		else if(hex_lookup[val] !== undefined){
+		else if(lookup[val] !== undefined){
 		//value should be represented as hex value
-			return hex_lookup[val];
+			return lookup[val];
 		}
 		else{
-			return getHex(val);
+			return getBaseX(base, val);
 		}
 	}
 
 	/**
- 	* Calculates the base 32 of a decimal(base 10) number.
- 	* @param {number} val The desired decimal number to be converted.
- 	* @return {string} Returns a string representation of a base 32 number.
+ 	* Creates a base number object containing decimal numbers and their corresponding values .
+ 	* @param {number} base, The base value to convert to.
+ 	* @param {number} val, The desired decimal number to be converted.
+ 	* @return {object} Returns an base number object.
  	*/
-	function getBase32(val){
-		var base32 = '';
-		
-		if(val>=0 && val<32){
-			base32+=lookupBase32(Math.floor(val));
-			base32+=" ";
-			return base32;
-		}
-		else{
-			var x = Math.floor(val/32);
-			var y = val%32;
-				
-			base32+=lookupBase32(x);
-			base32+=lookupBase32(y);
-			base32+=" ";
+	function getLookup(base, val){
+		var lookup ={};
+
+		switch(base){
+			case 16:
+				lookup = {10:'A', 11:'B', 12:'C', 13:'D', 14:'E', 15:'F'};
+				return lookup;				
+				break;
 			
-			return base32;
-		}
-	}
-
-	/**
- 	* Maps a given decimal(base 10) value with its corresponding base 32 representation.
- 	* @param {number} val The desired decimal number to map.
- 	* @return {string} Returns a single letter or number.
- 	*/
-	function lookupBase32(val){
-		var base32_lookup = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:'2', 27:'3', 28:'4', 29:'5', 30:'6', 31:'7'}
-
-		if(val<0){
-			console.log("Invalid input. Number must be positive");
-		}
-		else if(base32_lookup[val] !== undefined){
-			return base32_lookup[val];
-		}
-		else{
-			return getBase32(val);
-		}
-	}
-
-	/**
- 	* Calculates the base 64 of a decimal(base 10) number.
- 	* @param {number} val The desired decimal number to be converted.
- 	* @return {string} Returns a string representation of a base 32 number.
- 	*/
-	function getBase64(val){
-		var base64 = '';
-
-		if(val>=0 && val<64){
-			return lookupBase64(Math.floor(val));
-		}
-		else{
-			var x = Math.floor(val/64);
-			var y = val%64;
-				
-
-			base64+=lookupBase64(x);
-			base64+=lookupBase64(y);
-			base64+=" ";
+			case 32:
+				lookup = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:'2', 27:'3', 28:'4', 29:'5', 30:'6', 31:'7'};
+				return lookup;
+				break;
 			
-			return base64;
-		}
-	}
+			case 64:
+				lookup = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:'a', 27:'b', 28:'c', 29:'d', 30:'e', 31:'f', 32:'g', 33:'h', 34:'i', 35:'j', 36:'k', 37:'l', 38:'m', 39:'n', 40:'o', 41:'p', 42:'q', 43:'r',44:'s', 45:'t', 46:'u', 47:'v', 48:'w', 49:'x', 50:'y', 51:'z', 52:'0', 53:'1', 54:'2', 55:'3', 56:'4', 57:'5', 58:'6', 59:'7', 60:'8', 61:'9', 62:'+', 63:'/'};
+				return lookup;
+				break;
 
-	function lookupBase64(val){
-		var base64_lookup = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:'a', 27:'b', 28:'c', 29:'d', 30:'e', 31:'f', 32:'g', 33:'h', 34:'i', 35:'j', 36:'k', 37:'l', 38:'m', 39:'n', 40:'o', 41:'p', 42:'q', 43:'r',44:'s', 45:'t', 46:'u', 47:'v', 48:'w', 49:'x', 50:'y', 51:'z', 52:'0', 53:'1', 54:'2', 55:'3', 56:'4', 57:'5', 58:'6', 59:'7', 60:'8', 61:'9', 62:'+', 63:'/'};
-
-		if(val<0){
-			console.log("Invalid input. Number must be positive");
-		}
-		else if(base64_lookup[val] !== undefined){
-			return base64_lookup[val];
-		}
-		else{
-			return getBase64(val);
-		}
-	}
-
-	/**
- 	* Maps a given decimal(base 10) value with its corresponding base 64 representation.
- 	* @param {number} val The desired decimal number to map.
- 	* @return {string} Returns a single letter or number.
- 	*/
-	function lookupBase64(val){
-		var base64_lookup = {0:'A', 1:'B', 2:'C', 3:'D', 4:'E', 5:'F', 6:'G', 7:'H', 8:'I', 9:'J', 10:'K', 11:'L', 12:'M', 13:'N', 14:'O', 15:'P', 16:'Q', 17:'R', 18:'S', 19:'T', 20:'U', 21:'V', 22:'W', 23:'X', 24:'Y', 25:'Z', 26:'a', 27:'b', 28:'c', 29:'d', 30:'e', 31:'f', 32:'g', 33:'h', 34:'i', 35:'j', 36:'k', 37:'l', 38:'m', 39:'n', 40:'o', 41:'p', 42:'q', 43:'r',44:'s', 45:'t', 46:'u', 47:'v', 48:'w', 49:'x', 50:'y', 51:'z', 52:'0', 53:'1', 54:'2', 55:'3', 56:'4', 57:'5', 58:'6', 59:'7', 60:'8', 61:'9', 62:'+', 63:'/'};
-
-		if(val<0){
-			console.log("Invalid input. Number must be positive");
-		}
-		else if(base64_lookup[val] !== undefined){
-			return base64_lookup[val];
-		}
-		else{
-			return getBase64(val);
+			default:
+				console.log("Invalid Base. Acceptable bases are 16, 32, and 64.");
 		}
 	}
 
@@ -207,10 +149,10 @@ var tooltip = tooltipElements[0];
 			var base64 = '';
 
 			for(var i = 0; i<(rgb.length)-1; i++){
-				hex += getHex(rgb[i]); //QUESTION: i just removed the keyword var from this line. prior to doing that, the line didn't work, what exaclty was happening since i essentially redeclared hex every time?
 				binary += getBinary(rgb[i]);
-				base32 += getBase32(rgb[i]);
-				base64 += getBase64(rgb[i]);
+				base32 += getBaseX(32,rgb[i]);
+				base64 += getBaseX(64,rgb[i]);
+				hex += getBaseX(16,rgb[i]); //QUESTION: i just removed the keyword var from this line. prior to doing that the line didn't work, what exaclty was happening since i essentially redeclared hex every time?
 			}
 			
 			rgb = rgb[0]+" "+rgb[1]+" "+rgb[2];
@@ -219,16 +161,23 @@ var tooltip = tooltipElements[0];
 			
 			tooltip.innerHTML = innerHTML;
 			tooltip.style.display = 'inline';
-			tooltip.style.left = e.pageX+"px"; 
-			tooltip.style.top = e.pageY+"px";}, 300);
+			console.log(e.pageX+" "+e.pageY);
+
+				if(e.pageY-100 < 35){
+					tooltip.style.top = "35px";
+				}
+				else{
+					tooltip.style.top = e.pageY-100+"px"; 
+				}
+
+				if(e.pageX-100 < 55){
+					tooltip.style.left = "55px";
+				}
+				else{
+					tooltip.style.left = e.pageX-100+"px"; 
+				}
+			}, 300);
 		}
-
-	/*function calculateConversions(x, y, ctx){
-		getHex(ctx.getImageData(x,y,1,1).data);
-		calculateBinary(ctx.getImageData(x,y,1,1).data);
-		getBase32(ctx.getImageData(x,y,1,1).data);
-	}*/
-
 
 	document.addEventListener('mousemove', function(e){
 		if (e.target.nodeName == 'IMG') {
@@ -251,3 +200,30 @@ var tooltip = tooltipElements[0];
 		}
 	
 	})
+
+
+	/*function calculateConversions(x, y, ctx){
+		getHex(ctx.getImageData(x,y,1,1).data);
+		calculateBinary(ctx.getImageData(x,y,1,1).data);
+		getBase32(ctx.getImageData(x,y,1,1).data);
+	}*/
+
+	/*function convert(base, val){
+		var num = '';
+
+		if(val>=0 && val<base){
+			num+=lookup(base, Math.floor(val));
+			num+=" ";
+			return num;
+		}
+		else{
+			var x = Math.floor(val/base);
+			var y = val%base;
+
+			num+=lookup(base, x);
+			num+=lookup(base, y);
+			num+=" ";
+
+			return num;
+		}
+	}*/
